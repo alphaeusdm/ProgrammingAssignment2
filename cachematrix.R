720b1f5514effd3049a7e255381798a40e305f09
alphaeusdm <alphaeusdm27@gmail.com>
2020-05-08 10:28:16 +0530

    Initial commit for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
+## This function creates a list containing function to set and get a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  mi <- NULL
+  set <- function(y) {
+    x <<- y
+    mi <<- NULL
+  }
+  get <- function() x
+  setminverse <- function(minverse) mi <<- minverse
+  getminverse <- function() mi
+  list(set = set, get = get,
+       setminverse = setminverse,
+       getminverse = getminverse)
 }
 
 
 ## Write a short comment describing this function
 
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mi <- x$getminverse()
+  if(!is.null(mi)) {
+    message("getting cached data")
+    return(mi)
+  }
+  data <- x$get()
+  mi <- solve(data, ...)
+  x$setminverse(mi)
+  mi
 }
